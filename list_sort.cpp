077c08c540257ef15369f3ddd91362173be1389f
Andrew Wood <wood.andrew9@gmail.com>
2018-09-24 16:53:51 -0400

    Modified project to get to starting point.

--- list_sort.cpp
+++ list_sort.cpp
@@ -1,26 +1,38 @@
-#include <iostream>
-#include <vector.h>
-#include <gheap_advanced.h>
+/*
+    list_insertion_sort.cpp
+    05/17/17
+    Chris Lacher
 
-int main()
+    Implementation of List<T>::Sort() using the
+    insertion sort algorithm
+
+    Copyright 2017, R. C. Lacher
+*/
+
+//----------------------------------
+//     List<T>::Sort Implementations
+//----------------------------------
+
+template < typename T >
+template < class P >
+void List<T>::Sort (P& comp)
+// insertion sort: in place, stable, Theta(n*n)
 {
-  fsu::Vector<int> testList;
-
-  testList.PushBack(3);
-  testList.PushBack(2);
-  testList.PushBack(7);
-  testList.PushBack(9);
-  testList.PushBack(5);
-
-  fsu::Vector<int>::Iterator beg = testList.Begin();
-  fsu::Vector<int>::Iterator end = testList.End();
-
-  std::cout << "Before Heap Sort:\n";
-  std::cout << testList << "\n";
-
-  alt::g_heap_sort(beg,end);
-
-  std::cout << "After Heap Sort:\n";
-  std::cout << testList;
-  return 0;
+  Iterator i, j, k;
+  T t;
+  for (i = Begin(); i != End(); ++i)
+  {
+    t = *i;
+    for (k = i, j = k--; j != Begin() && comp(t,*k); --j, --k)
+      *j = *k;
+    *j = t;
+  }
 }
+
+template < typename T >
+void List<T>::Sort ()
+{
+  fsu::LessThan<T> p;
+  Sort(p);
+}
+
